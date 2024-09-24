116febb41c0bca54e5dc4f9ab1d56fcdde76ddfb
Robert Csakany <csakany.robert@gmail.com>
2024-09-24 15:10:52 +0200

    JNG-5937 Generator git ignore synchronizer glob ignore (#186)

--- generator-engine/src/main/java/hu/blackbelt/judo/meta/psm/generator/engine/PsmGenerator.java
+++ generator-engine/src/main/java/hu/blackbelt/judo/meta/psm/generator/engine/PsmGenerator.java
@@ -37,6 +37,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 import java.io.*;
+import java.nio.file.FileSystems;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -320,7 +323,17 @@ public class PsmGenerator {
                 .filter(genericParams.getDiscriminatorPredicate())
                 .collect(Collectors.toSet());
 
-        ModelGenerator.synchronizeGitignoreInDirectory(genericParams, applications,  (f) -> ignoredFiles.contains(f));
+        ModelGenerator.synchronizeGitignoreInDirectory(genericParams, applications, getIgnoredFileMatcher(ignoredFiles));
+    }
+
+    public static Function<String, Boolean> getIgnoredFileMatcher(Collection<String> ignoredGlobs) {
+        return path -> {
+            Boolean match = ignoredGlobs.stream().anyMatch((glob) -> {
+                final PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:" + glob);
+                return pathMatcher.matches(Paths.get(path));
+            });
+            return match;
+        };
     }
 
     public static void recalculateChecksumForDirectory(PsmGeneratorParameter.PsmGeneratorParameterBuilder builder) throws Exception {
